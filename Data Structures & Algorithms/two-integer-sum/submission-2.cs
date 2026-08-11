f8243e42333abaf152eb3310600cd81b2977b955
Z3rol <123364654+Z3rol@users.noreply.github.com>
2026-08-11 23:27:04 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,19 @@
+public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                var numToIndex = new Dictionary<int, int>();
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int complement = target - nums[i];
+
+                    if (numToIndex.TryGetValue(complement, out int index))
+                        return [index, i];
+
+                    numToIndex[nums[i]] = i;
+                }
+
+                return [0, 0];
+            }
+        }
