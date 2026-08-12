3ffa353fe7c6ba6172a6f6231cfb16f2895ba24d
Z3rol <123364654+Z3rol@users.noreply.github.com>
2026-08-13 00:37:01 +0300

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length == 1) return 1;
+
+        var substring = new HashSet<char>();
+
+        var longestSubstring = 0;
+
+        int l = 0;
+
+        for (int r = 0; r < s.Length; r++)
+        {
+            while (substring.Contains(s[r]))
+            {
+                substring.Remove(s[l]);
+                l++;
+            }
+
+            substring.Add(s[r]);
+            longestSubstring = Math.Max(longestSubstring, substring.Count);
+        }
+
+        return Math.Max(longestSubstring, substring.Count);
+    }
+}
