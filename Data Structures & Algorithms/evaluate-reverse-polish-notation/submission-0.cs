006585efc388b9d11e0d3f0f7d6c921ccda3b47b
Z3rol <123364654+Z3rol@users.noreply.github.com>
2026-08-17 23:25:07 +0300

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cs
@@ -0,0 +1,42 @@
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if ("+-*/".Contains(tokens[i]))
+            {
+                int secondVal = stack.Pop();
+                int firstVal = stack.Pop();
+                int result = 0;
+
+                switch (tokens[i])
+                {
+                    case "+":
+                        result = firstVal + secondVal;
+                        break;
+
+                    case "-":
+                        result = firstVal - secondVal;
+                        break;
+
+                    case "*":
+                        result = firstVal * secondVal;
+                        break;
+
+                    case "/":
+                        result = firstVal / secondVal;
+                        break;
+                }
+
+                stack.Push(result);
+            }
+            else
+                stack.Push(int.Parse(tokens[i]));
+        }
+
+        return stack.Pop();
+    }
+}
