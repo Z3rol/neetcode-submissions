478be1246e9b8a8deb6d6eecf6da421a45050ec0
Z3rol <123364654+Z3rol@users.noreply.github.com>
2026-08-19 23:58:08 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution
+{
+    public string SortString(string s)
+    {
+        char[] chars = s.ToCharArray();
+        Array.Sort(chars);
+        return new string(chars);
+    }
+
+    public List<List<string>> GroupAnagrams(string[] strs)
+    {
+        var result = new List<List<string>>();
+
+        var anagramGroups = new Dictionary<string, List<string>>();
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            string sortedStr = SortString(strs[i]);
+            
+            if (!anagramGroups.ContainsKey(sortedStr))
+            {
+                anagramGroups.Add(sortedStr, new List<string>());
+            }
+
+            anagramGroups[sortedStr].Add(strs[i]);
+        }
+
+        foreach (var pair in anagramGroups)
+        {
+            result.Add(pair.Value);
+        }
+
+        return result;
+    }
+}
