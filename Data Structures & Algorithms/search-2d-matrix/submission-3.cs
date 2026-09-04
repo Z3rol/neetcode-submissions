63d98e3d0ba6e8ca35a2f54867d661ca8e117ea1
Z3rol <123364654+Z3rol@users.noreply.github.com>
2026-09-04 07:52:08 +0300

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+
+        int l = 0;
+        int r = (rows * cols) - 1;
+
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+
+            int mNum = matrix[m / cols][m % cols];
+
+            if (mNum == target)
+            {
+                return true;
+            }
+            else if (mNum > target)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return false;
+    }
+}
